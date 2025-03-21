05ea99ebf31b425a51359b7c67e7b28f5fc260db
codemaker-kim <tnals72441@daum.net>
2025-03-21 19:42:41 +0900

    Setting: "프로젝트 환경 설정"

---
+++ src/main/java/org/project/basicboard/BasicboardApplication.java
@@ -0,0 +1,13 @@
+package org.project.basicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicboardApplication.class, args);
+    }
+
+}
