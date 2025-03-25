a32564d29d41b0f14553aaafc47c8adab4819e60
codemaker-kim <tnals72441@daum.net>
2025-03-25 22:11:15 +0900

    Chore: "코드 포맷팅 수정"

--- src/main/java/org/project/basicboard/user/api/dto/request/UserJoinRequest.java
+++ src/main/java/org/project/basicboard/user/api/dto/request/UserJoinRequest.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 @Builder
 public record UserJoinRequest(
         @NotEmpty(message = "아이디가 비어있습니다.") String username,
-        @NotEmpty(message = "패스워드가 비어있습니다. ") String password,
+        @NotEmpty(message = "패스워드가 비어있습니다.") String password,
         @NotEmpty(message = "닉네임이 비어있습니다.") String nickname
 ) {
 }
