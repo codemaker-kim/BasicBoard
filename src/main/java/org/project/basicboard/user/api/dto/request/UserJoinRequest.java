6167adbf5e3856425dd4942a6f2f3d48c9127dbc
codemaker-kim <tnals72441@daum.net>
2025-03-25 19:48:14 +0900

    Feat: "dto에 에러 메시지 추가"

--- src/main/java/org/project/basicboard/user/api/dto/request/UserJoinRequest.java
+++ src/main/java/org/project/basicboard/user/api/dto/request/UserJoinRequest.java
@@ -1,13 +1,12 @@
 package org.project.basicboard.user.api.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record UserJoinRequest(
-        @NotEmpty String username,
-        @NotEmpty String password,
-        @NotEmpty String nickname
+        @NotEmpty(message = "아이디가 비어있습니다.") String username,
+        @NotEmpty(message = "패스워드가 비어있습니다. ") String password,
+        @NotEmpty(message = "닉네임이 비어있습니다.") String nickname
 ) {
 }
