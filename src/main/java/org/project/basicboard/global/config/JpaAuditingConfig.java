4eb1bd9a67605c24862831135ae5518bcb83af8a
codemaker-kim <tnals72441@daum.net>
2025-03-25 18:37:11 +0900

    Feat: "JPA 설정 추가"

---
+++ src/main/java/org/project/basicboard/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.project.basicboard.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
