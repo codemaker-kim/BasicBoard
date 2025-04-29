309518777032136d11fca49fc7efdcdfabbd90a6
codemaker-kim <tnals72441@daum.net>
2025-04-30 01:52:39 +0900

    Fix: "컬럼명 설정 추가"

--- src/main/java/org/project/basicboard/user/domain/User.java
+++ src/main/java/org/project/basicboard/user/domain/User.java
@@ -36,7 +36,7 @@ public class User {
     private Role role;
 
     @CreatedDate
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Column(unique = true)
