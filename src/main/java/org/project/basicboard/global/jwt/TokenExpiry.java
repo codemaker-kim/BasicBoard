5e4c32146cc96f257076c78a70786b4ffdb1e4cd
codemaker-kim <tnals72441@daum.net>
2025-04-29 19:46:26 +0900

    Refactor: "토큰 만료일 주석 추가"

--- src/main/java/org/project/basicboard/global/jwt/TokenExpiry.java
+++ src/main/java/org/project/basicboard/global/jwt/TokenExpiry.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TokenExpiry {
-    ACCESS_TOKEN_EXPIRY_TIME(60 * 60 * 1000L),
-    REFRESH_TOKEN_EXPIRY_TIME(14 * 24 * 60 * 60 * 1000L);
+    ACCESS_TOKEN_EXPIRY_TIME(60 * 60 * 1000L), // 1시간
+    REFRESH_TOKEN_EXPIRY_TIME(14 * 24 * 60 * 60 * 1000L); // 14일
 
     final Long expiry;
 }
