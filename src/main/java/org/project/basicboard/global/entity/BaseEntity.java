3e1c51025ae9720a60047278feddcd249343c7fe
codemaker-kim <tnals72441@daum.net>
2025-04-30 01:03:25 +0900

    Refactor: "Auditing 역할 분리를 위한 BaseEntity 분리"

--- src/main/java/org/project/basicboard/global/entity/BaseEntity.java
+++ src/main/java/org/project/basicboard/global/entity/BaseEntity.java
@@ -2,26 +2,14 @@ package org.project.basicboard.global.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public abstract class BaseEntity extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(name = "update_at", nullable = false)
-    private LocalDateTime updatedAt;
 }
