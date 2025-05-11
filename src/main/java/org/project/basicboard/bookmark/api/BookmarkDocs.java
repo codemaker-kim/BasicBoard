9864b1dae5297a7aba8033f6508b2edfcbe4bec5
codemaker-kim <tnals72441@daum.net>
2025-05-12 03:10:38 +0900

    Feat: "북마크, 좋아요 스웨거 오퍼레이션 추가"

--- src/main/java/org/project/basicboard/bookmark/api/BookmarkDocs.java
+++ src/main/java/org/project/basicboard/bookmark/api/BookmarkDocs.java
@@ -1,4 +1,4 @@
-package org.project.basicboard.likes.controller;
+package org.project.basicboard.bookmark.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -6,28 +6,28 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
-@Tag(name = "[좋아요 API]",  description = "좋아요 관련 API")
-interface LikesDocs {
-    @Operation(summary = "좋아요 생성", description = "게시글에 좋아요를 추가합니다.",
+@Tag(name = "[북마크 API]",  description = "북마크 관련 API")
+public interface BookmarkDocs {
+    @Operation(summary = "북마크 생성", description = "게시글에 북마크를 추가합니다.",
             responses = {
-                    @ApiResponse(responseCode = "204", description = "좋아요 생성 성공"),
+                    @ApiResponse(responseCode = "204", description = "북마크 생성 성공"),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "403", description = "인증 실패"),
                     @ApiResponse(responseCode = "500", description = "서버 오류")
             })
-    ResponseEntity<Void> createLike(
+    ResponseEntity<Void> createBookmark(
             @Parameter(description = "토큰에서 추출한 사용자명", hidden = true) String username,
             @Parameter(description = "게시글 아이디", required = true) Long articleId);
 
-    @Operation(summary = "좋아요 삭제", description = "게시글 좋아요를 삭제합니다.",
+    @Operation(summary = "북마크 삭제", description = "게시글에서 북마크를 삭제합니다.",
             responses = {
-                    @ApiResponse(responseCode = "204", description = "좋아요 취소 성공"),
+                    @ApiResponse(responseCode = "204", description = "북마크 삭제 성공"),
                     @ApiResponse(responseCode = "400", description = "잘못된 요청"),
                     @ApiResponse(responseCode = "403", description = "인증 실패"),
-                    @ApiResponse(responseCode = "404", description = "좋아요를 찾을 수 없음."),
+                    @ApiResponse(responseCode = "404", description = "북마크를 찾을 수 없음."),
                     @ApiResponse(responseCode = "500", description = "서버 오류")
             })
-    ResponseEntity<Void> deleteLike(
+    ResponseEntity<Void> deleteBookmark(
             @Parameter(description = "토큰에서 추출한 사용자명", hidden = true) String username,
             @Parameter(description = "게시글 아이디", required = true) Long articleId);
 }
