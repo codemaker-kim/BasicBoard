28f468f3c4406c2e2420d5eaafc4dcfc7e46e9d0
codemaker-kim <tnals72441@daum.net>
2025-05-14 00:39:18 +0900

    Chore: "코드 포맷팅 수정"

--- src/main/java/org/project/basicboard/likes/repository/LikesRepository.java
+++ src/main/java/org/project/basicboard/likes/repository/LikesRepository.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-
     Optional<Likes> findByArticleIdAndUsername(Long articleId, String username);
 
     boolean existsByArticleIdAndUsername(Long articleId, String username);
-
-    Long countByArticleId(Long articleId);
 }
